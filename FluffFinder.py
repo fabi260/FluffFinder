2f0bc60d982e51b76c4f1f621c0f67665850101a
fabi260 <fabian-g-sauer@web.de>
2024-04-26 14:38:53 +0200

    Initial prompt and concept definitions

--- FluffFinder.py
+++ FluffFinder.py
@@ -6,14 +6,20 @@ load_dotenv()
 api_key = os.getenv('OPENAI_API_KEY')
 client = OpenAI(api_key=api_key)
 
-base_instruction = "Your role is to evaluate texts on their Fluff content. Fluff is defined as content that is unnecessary, redundant, or overly wordy. Your purpose is to score the text on its fluff content on a scale from 1 (a lot of fluff) to 5 (no fluff). Your only output is the score. (1-5)"
+base_instruction = "Your purpose is to evaluate and score text on their fluff content on a scale from 1 (a lot of fluff) to 5 (no fluff). Your only output is the score. (1-5)"
+fluff_definition = "Fluff is defined as content that is unnecessary, redundant, or overly wordy."
+interest_categoriess = {
+        'General Interest': 'Written to appeal to a broad audience, covering topics intended to engage or entertain readers without requiring specialized knowledge.',
+        'Special Interest': 'Written to appeal to a pre-informed audience with personal interest, providing information required for decision making.',
+        'Professional Interest': 'Written to appeal to a professional audience with deep prior knowledge, describing products and services for professional use.'
+        }
 
 
 def vanilla_fluff(text):
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": base_instruction},
+            {"role": "system", "content": base_instruction + fluff_definition},
             {"role": "user", "content": text}
         ]
     )
