2f1f3b4e971bde8dd3744f607f7218b84c2e4f73
fabi260 <fabian-g-sauer@web.de>
2024-04-29 14:04:19 +0200

    test_FluffFinder as a Jupyter notebook

--- test_FluffFinder.py
+++
@@ -1,7 +0,0 @@
-from FluffFinder import vanilla_fluff
-
-text = 'Filmmaking tools to support your creative vision. Unleash your creative instincts, no matter your project or budget. Our cameras and lenses give you the power to shoot a documentary, experimental short film or an entire feature. Produce captivating work with full-frame sensors, built-in filters, professional codecs and more.'
-
-score = vanilla_fluff(text)
-
-print(score)
