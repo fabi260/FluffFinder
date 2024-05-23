d2640ace90dbc3cb1f1d21fea2cb66419182b3ad
fabi260 <fabian-g-sauer@web.de>
2024-05-23 15:45:23 +0200

    Comments and clean up

--- FluffFinder.py
+++ FluffFinder.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 import os
-from openai import OpenAI
+from openai import OpenAI, tiktoken
 
 load_dotenv()
 api_key = os.getenv('OPENAI_API_KEY')
@@ -16,6 +16,8 @@ FLUFF_SCORES = "1 (no Fluff), 2 (Little Fluff), 3 (Some Fluff), 4 (Considerable 
 VANILLA_SCORE_INSTRUCTION = f"Provide a numerical Fluff score for this text according to the following scale: {FLUFF_SCORES} Your only output is the score (1-5)."
 VANILLA_REASON_INSTRUCTION = "Provide a concise reason for your score in less than 15 words based on the text, your score and your understanding of Fluff."
 
+## TODO: Add other Evaluators within a class
+
 def get_gpt_completion(messages):
     try:
         completion = client.chat.completions.create(
@@ -27,6 +29,25 @@ def get_gpt_completion(messages):
     except Exception as e:
         print(f"Error: {e}")
         return None
+
+def num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613"): # note: future models may deviate from this, from https://platform.openai.com/docs/guides/text-generation/managing-tokens
+  """Returns the number of tokens used by a list of messages."""
+  try:
+      encoding = tiktoken.encoding_for_model(model)
+  except KeyError:
+      encoding = tiktoken.get_encoding("cl100k_base")
+  if model == "gpt-3.5-turbo-0613":  # note: future models may deviate from this
+      num_tokens = 0
+      for message in messages:
+          num_tokens += 4  # every message follows <im_start>{role/name}\n{content}<im_end>\n
+          for key, value in message.items():
+              num_tokens += len(encoding.encode(value))
+              if key == "name":  # if there's a name, the role is omitted
+                  num_tokens += -1  # role is always required and always 1 token
+      num_tokens += 2  # every reply is primed with <im_start>assistant
+      return num_tokens
+  else:
+      raise NotImplementedError(f"""num_tokens_from_messages() is not presently implemented for model {model}.""")
 
 def vanilla_fluff(text): ##TODO: Add possibility to only get score
     """
